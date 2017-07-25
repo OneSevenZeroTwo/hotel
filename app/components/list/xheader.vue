3c37952e9ea18db22b2c5d307b2bc33c664af07e
古龙 <1250552871@qq.com>
2017-07-25 18:08:56 +0800

    list

---
+++ app/components/list/xheader.vue
@@ -0,0 +1,24 @@
+<tamplate>
+	<header class="bar bar-nav hotel-list-header">
+    	<a class="icon pull-left icon-back tjclick" data-rel="back" data-href="https://m.elong.com/?t=1500974043146" data-tj="{&quot;cspot&quot;:&quot;back&quot;}"></a>
+    	<a class="icon icon-nearby pull-right tjclick" data-tj="{&quot;cspot&quot;:&quot;map&quot;}"></a>
+    	<a class="icon icon-collectlist  collect pull-right tjclick" href="//m.elong.com/my/hotel/favorites/" data-tj="{&quot;cspot&quot;:&quot;mycollection&quot;}"></a>
+    	<h1 class="title">
+    		<span class="city-area">
+    		<span class="city">
+    			北京市
+    		</span>
+    		<span class="num" style="display:none;"></span>
+    		<i class="icon-bottom"></i>
+    		</span>
+   		</h1>
+	</header>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+</style>
--- app/main.js
+++ app/main.js
@@ -39,6 +39,8 @@ import topics from "./components/routes/topics.vue"
 import good from "./components/routes/good.vue"
 import detail from "./components/routes/detail.vue"
 import muse from "./components/muse/test.vue"
+//详情页路由
+import list from "./components/list/router/list.vue"
 
 
 //把定义好的路由组件引进来放到component中，path为进入路由的名字，然后等待路由实例化(new VueRouter)。
@@ -80,6 +82,12 @@ var routes = [{
 		//上面定义好的路由扔进来
 		component: muse
 	},
+	//列表页开始
+	{
+		path:'/list',
+		component:list
+	},
+	//列表页结束
 	{
 		//重定向，没有路由时页面默认加载/index/topics路由
 		path: '/',
