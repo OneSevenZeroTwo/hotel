30c6521cf37fb71484c3d32238932cc522134c46
as13579e <1104063336@qq.com>
2017-08-01 11:17:33 +0800

    list

--- app/components/list/clockhotel/xheader.vue
+++ app/components/list/clockhotel/xheader.vue
@@ -2,7 +2,7 @@
 	<header class="bar bar-nav hotel-list-header">
         <!--<a class="icon pull-left icon-back" href="javascript:void(0)" method="goHome" data-href="/clockhotel/search#inDate=2017-07-30"></a>-->
         <i class="material-icons" style="position: relative; font-size: 39px; top: 3px; left: -7px;">&#xE314;</i>
-        <h1 class="title">
+        <h1 class="title" style="top: 0px;">
         	<span class="city-area">
 				<span class="city">{{cityName}}</span>
         	</span>
