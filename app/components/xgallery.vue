38de3ebe06bb7453bead99fecb637f733932043f
chuanyeniubi <851792143@qq.com>
2017-08-01 09:26:36 +0800

    首页

--- app/components/xgallery.vue
+++ app/components/xgallery.vue
@@ -3,8 +3,8 @@
 		<span class="weui-gallery__img" :style="{backgroundImage: 'url('+indexImg+')'}"></span>
 		<div class="weui-gallery__opr">
 			<a @click="hideGallery" href="javascript:" class="weui-gallery__del">
-				<i class="weui-icon-delete weui-icon_gallery-delete"></i>
-				<!--<i class="material-icons">&#xE5C9;</i>-->
+				<!--<i class="weui-icon-delete weui-icon_gallery-delete"></i>-->
+				<i class="material-icons" style=" top: 11px;color: #fff;font-size: 39px;position: relative;">&#xE5C9;</i>
 			</a>
 		</div>
 	</div>
