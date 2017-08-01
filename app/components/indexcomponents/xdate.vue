8003f1199b4078906c793cf556b5cea667ecaf00
as13579e <1104063336@qq.com>
2017-08-01 12:30:19 +0800

    区域参数

--- app/components/indexcomponents/xdate.vue
+++ app/components/indexcomponents/xdate.vue
@@ -21,18 +21,15 @@
 				
 				//把时间中的/变为-
 				var Inyear = this.datee[0].getFullYear()
-				var INmonths = (''+this.datee[0].getMonth()).length<2?"0"+this.datee[0].getMonth():this.datee[0].getMonth()
+				var INmonths = (''+this.datee[0].getMonth()).length<2?"0"+(this.datee[0].getMonth()+1):this.datee[0].getMonth()+1
 				var INday = (''+this.datee[0].getDate()).length<2?"0"+this.datee[0].getDate():this.datee[0].getDate()
 				scope.listFilterParams.indate = Inyear + "-"+INmonths+"-"+INday
-				
+				scope.hotelInformation.starTime = Inyear + "-"+INmonths+"-"+INday
 				var Outyear = this.datee[1].getFullYear()
-				var Outmonths = (''+this.datee[1].getMonth()).length<2?"0"+this.datee[1].getMonth():this.datee[1].getMonth()
+				var Outmonths = (''+this.datee[1].getMonth()).length<2?"0"+(this.datee[1].getMonth()+1):this.datee[1].getMonth()+1
 				var Outday = (''+this.datee[1].getDate()).length<2?"0"+this.datee[1].getDate():this.datee[1].getDate()
 				scope.listFilterParams.outdate = Outyear + "-"+Outmonths+"-"+Outday
-				console.log(Inyear,INmonths,INday)
-				console.log(Outyear,Outmonths,Outday)
-				
-				console.log(scope.listFilterParams.indate,scope.listFilterParams.outdate)
+				scope.hotelInformation.leaveTime = Inyear + "-"+INmonths+"-"+INday
 			},
 
 		},
